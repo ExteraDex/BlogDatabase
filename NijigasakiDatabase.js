c18287501dd28c11ccd12969a6b9d5b71a53ddd2
IkhsaN D <omegaexteravol@gmail.com>
2022-04-16 16:11:55 +0700

    Add files via upload

---
+++ NijigasakiDatabase.js
@@ -0,0 +1,37 @@
+// Photo Utama Postingan //
+var rillBang = "https://i.imgur.com/APb6N0j.jpg";
+// DataBase Musik //
+var xrayData = [
+    
+    {name:"Muteki-kyuu*Believer | Mirai Harmony",
+     date:"July 29, 2020",
+     link:"https://daloadmusic.blogspot.com/2020/07/nijigasaki-high-school-idol-clubs.html"},
+    {name:"DiverDiva – SUPER NOVA",
+     date:"February 12, 2020",
+     link:"https://daloadmusic.blogspot.com/2020/02/diverdiva-super-nova.html"},
+    {name:"A·ZU·NA – Dream Land! Dream World!",
+     date:"February 12, 2020",
+     link:"https://daloadmusic.blogspot.com/2020/02/azuna-dream-land-dream-world.html"},
+    {name:"QU4RTZ – Sing & Smile!!",
+     date:"February 12, 2020",
+     link:"https://daloadmusic.blogspot.com/2020/02/qu4rtz-sing-smile.html"},
+    {name:"Love U my friends - Nijigasaki High School Idol Club",
+     date:"October 2, 2019",
+     link:"https://daloadmusic.blogspot.com/2019/11/love-u-my-friends-nijigasaki-high.html"},
+    {name:"Love Live! Nijigasaki High ~Ohiruyasumi Housoushitsu~ Drama CD: Houkago Fantasia",
+     date:"Mei 22, 2019",
+     link:"https://daloadmusic.blogspot.com/2020/07/love-live-nijigasaki-high-ohiruyasumi.html"},
+    {name:"TOKIMEKI Runners - Nijigasaki High School Idol Club",
+     date:"November 21, 2018",
+     link:"https://daloadmusic.blogspot.com/2019/11/tokimeki-runners-nijigasaki-high-school.html"},
+ ];
+// Database Music Vidio//
+var myMv = [
+    
+    {name:"Mirai Harmony MV",
+     date:"July 29, 2020",
+     link:"https://daloadmusic.blogspot.com/2020/07/mv-nijigasaki-high-school-idol-club.html"},
+    {name:"Muteki-kyuu*Believer MV",
+     date:"July 29, 2020",
+     link:"https://daloadmusic.blogspot.com/2020/07/mv-nijigasaki-high-school-idol-clubs.html"},
+]
